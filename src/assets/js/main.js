0139a8da2c938a28ff5d26e9a1f3e5206f4c7905
Wesley Rodrigues dos Reis <wesleyepolly16@gmail.com>
2022-02-20 12:56:32 -0300

    criando logica de verificacao

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -27,6 +27,16 @@ let countSilab = document.querySelector("#silab-count")
 // search
 let inputSearch = document.querySelector("#inputSearch")
 
+// letter error
+let letterError = document.querySelector("#letter-error")
+
+//count acert
+let countAdd = document.querySelector("#count-acert")
+
+//audio
+let audioError = document.querySelector("#audioError");
+let audioWin = document.querySelector("#audioWin");
+let audioFinish = document.querySelector("#audioFinish");
 
 //secret text
 var secret =''
@@ -50,20 +60,24 @@ btnAddText.addEventListener('click', () => {
 btnCancel.addEventListener('click', () => {
     openClose(pagHome, pagAddText)
     openClose('',divSearch)
+    letterError.classList.add('hidden')
 })
 
 //play game
 btnPlay.addEventListener('click', () => {
     startGame()
     openClose(pagPlay, pagHome)
-    openClose(divSearch, null)
+    openClose(divSearch)
+    letterError.classList.add('hidden')
+    inputSearch.focus()
 })
 //cancel game
 btnCancelPlay.addEventListener('click', () => {
     clearFrom()
     stopGame()
     openClose(pagHome, pagPlay)
-    openClose(null,divSearch)
+    letterError.classList.add('hidden')
+    openClose('',divSearch)
 })
 
 //add text and init game
@@ -112,7 +126,9 @@ btnAddPlay.addEventListener('click', () => {
             clearFrom()
             startGame()
             openClose(pagPlay, pagAddText)
-            openClose(divSearch, null)
+            openClose(divSearch)
+            letterError.classList.add('hidden')
+            inputSearch.focus()
         }, 2000)
 
    
@@ -124,27 +140,111 @@ btnAddPlay.addEventListener('click', () => {
 btnNewPlay.addEventListener('click', () => {
     clearFrom()
     startGame()
-    openClose(divSearch,null)
+    openClose(divSearch)
+    letterError.classList.add('hidden')
+    inputSearch.focus()
 })
 
 //verify we leter exists in text
 function verifyLeter(){
-    var leter = inputSearch.value.toUpperCase()
-    countLeter = 0
-    pos = secret.indexOf(leter)
-    ok = secret.indexOf(leter) !== -1
+    var letter = inputSearch.value.toUpperCase()
+    var countLetter = 0
+    pos = secret.indexOf(letter)
+    res = secret.indexOf(letter) !== -1
 
+    // adiciona letras na posição correta do elemento em relaçao ao pai
     while(pos != -1){
-        countLeter ++
-        countSilab.children[pos].textContent = leter
-        pos = secret.indexOf(leter, pos +1)
+        countLetter ++
+        countSilab.children[pos].textContent = letter
+        pos = secret.indexOf(letter, pos +1)
+
+        addLetter(letter)
     }
-    console.log(secret)
-    console.log(ok)
+    inputSearch.value = ''
+    inputSearch.focus()
 
-
+    error(res, letter)
 }
 
+
+//retorna erro de escolha da letra
+function error(res, letter){
+    if (res === true) {
+        var totalLetter = countSilab.children.length
+        var totalAcept = countAdd.children.length
+        console.log(secret)
+        if(totalAcept === totalLetter){
+            somWin()
+            openClose('', divSearch)
+
+            message.classList.remove('hidden')
+            message.innerHTML = '<p class="text-4xl font-bold text-[#608C62]">YOU WIN</p>'
+
+            setTimeout(() => {
+                message.classList.add('hidden')
+            }, 5000)
+        }
+        return
+    }
+    letterError.classList.remove('hidden')
+
+    var p = document.createElement("p")
+    p.className="text-red-500 text-base font-bold" 
+    p.textContent = letter.toUpperCase()
+    letterError.appendChild(p)
+
+    renderError()
+}
+
+function addLetter(valor){
+    var letA = document.createElement("p")
+    letA.className="border-b-4 font-medium" 
+    letA.textContent = valor.toUpperCase()
+    countAdd.appendChild(letA)
+}
+
+//renderizar som e imagem final
+function renderError(){
+    var totalError = letterError.children.length
+    if(totalError < 7){
+       somError()
+    }else{
+        somFinish()
+        imageFinish()
+        openClose('',divSearch)
+
+        message.classList.remove('hidden')
+        message.innerHTML = '<p class="text-4xl font-bold text-[#AE5336]">YOU LOSE</p>'
+
+        setTimeout(() => {
+            message.classList.add('hidden')
+        }, 5000)
+    }
+}
+
+function somError(){
+    audioError.play()
+}
+
+function somFinish(){
+    audioFinish.play()
+}
+
+function somWin(){
+    audioWin.play()
+}
+
+function imageInit(){
+    //
+}
+
+function imageFinish(){
+    console.log("imagem de finish")
+}
+
+function imageWin(){
+    //
+}
 
 //função para abrir e fechar página
 function openClose(pagOpen=null, pagClose=null){
@@ -155,14 +255,7 @@ function openClose(pagOpen=null, pagClose=null){
     if(pagClose){
         pagClose.classList.add('hidden')
         pagClose.classList.remove('flex')
-    }else{
-        pagOpen.classList.add('flex')
-        pagOpen.classList.remove('hidden')
-    
-        pagClose.classList.add('hidden')
-        pagClose.classList.remove('flex')
     }
-
 }
 
 function startGame(){
@@ -178,6 +271,14 @@ function clearFrom(){
     while(countSilab.hasChildNodes()){
         countSilab.removeChild(countSilab.lastChild)
     }
+
+    while(letterError.hasChildNodes()){
+        letterError.removeChild(letterError.lastChild)
+    }
+
+    while(countAdd.hasChildNodes()){
+        countAdd.removeChild(countAdd.lastChild)
+    }
 }
 //text random
 function sortText(){
@@ -188,7 +289,6 @@ function sortText(){
     for(let index=0; index < text.length; index++) {
         var p = document.createElement("p")
         p.className="border-b-4 border-[#86734d] px-3 text-gray-50 text-xl font-medium" 
-        //p.textContent = text[index].toUpperCase()
         countSilab.appendChild(p)
     }
 
